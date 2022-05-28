f0d335634ee44974b1caae2a5b384f6a89d1581d
James Fincher <heartandkarma@gmail.com>
2022-05-28 00:00:40 -0700

    Init upload, working with comments

---
+++ person_picker.py
@@ -0,0 +1,13 @@
+import random
+
+#Get names from user
+name_string = input("Please supply names, each seperated by comsas... For example \n Tom, Jason, Carl, Sarah \n \n Names:")
+
+#Convert names to an easy to access list
+parsed_names = name_string.split(", ")
+
+#Select and set a random name from the list
+random_name = random.choice(parsed_names)
+
+#Return the random name to the user
+print(f"\n The random name selected is {random_name}")
